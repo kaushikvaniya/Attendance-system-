588866c37e60d22d4f06e032c251688a82ffa5f2
kaushikvaniya <kaushikvaniya907@gmail.com>
2024-02-26 19:32:16 +0530

    Header added

---
+++ attendance-system/src/Components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import Headercss from "../Header/Header.css";
+
+export default function Header() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Dashboard</a>
+          </li>
+          <li>
+            <a href="#">Class schedule</a>
+          </li>
+          <li>
+            <a href="#">View studends</a>
+          </li>
+          <li>
+            <a href="#">Take Attendance</a>
+          </li>
+          <li>
+            <a href="#">View Class Attendance</a>
+          </li>
+          <li>
+            <a href="#">Log in</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
